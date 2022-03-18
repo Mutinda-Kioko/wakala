11a6ae89f73efcd8f565b53dd2d04fb0a5436deb
Mutinda-Kioko <kiokojohn2275@gmail.com>
2022-03-18 10:24:18 +0300

    Added screens

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -5,11 +5,23 @@ import HideCard from '../components/HideCard';
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import SendScreen from './SendScreen';
+import AddScreen from './AddScreen';
 
 const Tab = createBottomTabNavigator();
 export default function MainScreen(props) {
   return (
     <NavigationContainer>
+    <Tab.Navigator>
+        <Tab.Screen name='Home' component={HomeScreen}></Tab.Screen>
+        <Tab.Screen name='Send' component={SendScreen}></Tab.Screen>
+        <Tab.Screen name='Add/Withdraw' component={AddScreen}></Tab.Screen>
+    </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+function HomeScreen() {
+    return(
     <View>
     <TouchableOpacity
     style={{alignItems:'flex-start', margin:16}}
@@ -26,10 +38,9 @@ export default function MainScreen(props) {
       </View>
     </View>
     </View>
-    </NavigationContainer>
-  );
+    )
+    
 }
-
 const styles = StyleSheet.create({
   container: {
     backgroundColor:"rgba(252,248,237,1)",
